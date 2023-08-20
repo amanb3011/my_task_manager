21a36169b85f6140abcb66d587dee0bcc1a4b0f9
amanb3011 <aman.bhandari.3011@gmail.com>
2023-08-20 22:09:32 +0530

    pushing to github

---
+++ config/mongoose.js
@@ -0,0 +1,22 @@
+// ---Taken these lines from mongoose documentation--
+
+// Require the Library
+const mongoose = require ('mongoose');
+
+// Connect to DB
+mongoose.connect('mongodb://localhost/todo_list', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// mongoose.connect("mongodb+srv://saumya2828:9871535223@cluster0.zovc5.mongodb.net/saumya-todo-db?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false ,useCreateIndex:true}).then(()=>{
+//     console.log("Mongodb connected");
+// });
+
+//Acquire the connection (to check if it is succesful)
+const db = mongoose.connection;
+
+//error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//up and running then print the message 
+db.once('open', function() {
+   console.log("we're connected!");
+});
